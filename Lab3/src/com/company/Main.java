ab5878a32d205b5b27a14679258192b5206aed74
Ilya <ilya.solovyiov@gmail.com>
2019-09-29 18:20:26 +0300

    Fixed edge case

--- Lab3/src/com/company/Main.java
+++ Lab3/src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
-        System.out.println(str.replaceAll("\\b [0-9]+\\b", ""));
+        System.out.println((" " + str + " ").replaceAll(" \\b[0-9]+\\b ", ""));
     }
 }
