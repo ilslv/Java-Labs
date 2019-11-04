9e0eacfded6d0fe0a6a8d9fdf530e0a764b9ed12
Ilya <ilya.solovyiov@gmail.com>
2019-11-04 20:16:39 +0300

    Fixed \n capturing in // comment

--- DeleteComments/src/com/company/Main.java
+++ DeleteComments/src/com/company/Main.java
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         String code = readFromFile("input.txt");
         System.out.println(
-                code.replaceAll("(\".*?\")|('.*?')|((//.*?\\n)|(/\\*(.|\\n)*?\\*/))", "$1$2")
+                code.replaceAll("(\".*?\")|('.*?')|((//.*?(?=\\n))|(/\\*(.|\\n)*?\\*/))", "$1$2")
         );
     }
 }
