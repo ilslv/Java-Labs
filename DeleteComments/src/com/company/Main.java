0f0250d9d77e192f1cb7e4da564da23c65ff33fb
Ilya <ilya.solovyiov@gmail.com>
2019-11-04 20:19:24 +0300

    Redundant imports have been removed

--- DeleteComments/src/com/company/Main.java
+++ DeleteComments/src/com/company/Main.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Main {
